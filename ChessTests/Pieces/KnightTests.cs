966ba832d2982e621244dc4c9aa0ffb58a5b448b
gabbe1993 <gabriel.vilen@gmail.com>
2016-04-03 19:49:21 +0200

    Knight test

---
+++ ChessTests/Pieces/KnightTests.cs
@@ -0,0 +1,31 @@
+﻿using Chess.Game;
+using Chess.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chess.Pieces.Tests
+{
+    [TestClass()]
+    public class KnightTests
+    {
+        [TestMethod()]
+        public void CanMoveToTest()
+        {
+            Piece knight = new Knight(Enums.Color.Black);
+            knight.CurrSquare = new Square(4, 4);
+
+            if (!knight.CanMoveTo(new Square(2, 3))) Assert.Fail();
+            if (!knight.CanMoveTo(new Square(2, 5))) Assert.Fail();
+
+            if (!knight.CanMoveTo(new Square(3, 2))) Assert.Fail();
+            if (!knight.CanMoveTo(new Square(3, 6))) Assert.Fail();
+
+            if (!knight.CanMoveTo(new Square(5, 2))) Assert.Fail();
+            if (!knight.CanMoveTo(new Square(5, 6))) Assert.Fail();
+
+            if (!knight.CanMoveTo(new Square(6, 3))) Assert.Fail();
+            if (!knight.CanMoveTo(new Square(6, 5))) Assert.Fail();
+
+            if (knight.CanMoveTo(new Square(6, 6))) Assert.Fail();
+        }
+    }
+}
